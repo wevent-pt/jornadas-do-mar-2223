305796470707372add8df48708df3cdd675b47a5
wevent-pt <wevent.pt@gmail.com>
2022-10-25 21:33:05 +0100

    eslint disable

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = {
     domains: ['gravatar.com']
   },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
     dirs: [
       'components',
       'layouts',
